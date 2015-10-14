f734c29a9726b64ba4a2fbe1cdff4eadaaa12211
aroswell <aroswell@gmail.com>
2015-10-14 14:58:56 -0400

    First commit of the moyenda slider project.

---
+++ moyenda.js
@@ -0,0 +1,119 @@
+$(document).on('ready page:load', function(){
+
+  // var slides = $(".phoenix .slide");
+  var slideObject = {
+    slides: slides
+  };
+
+  // slides.slideshow();
+
+});
+
+var interval;
+var slideImages = [
+  "images/bmw-e90-z-performance.jpg",
+  "images/ferrari_dino_1280x768.jpg",
+  "images/iss_art01_1280x768.jpg",
+  "images/Mar-Saura-3-1280x768.jpg",
+  "images/shipwreck-beach-zakynthos-greece-wallpaper-1280x768-405.jpg",
+  "images/ngc346_01_1280x768.jpg",
+  "images/tropical-beach-wallpaper-1280x768-144.jpg"
+];
+
+( function ($) {
+
+  $.fn.slideshow = function(slideOptions, action) {
+
+    var slides = this;
+
+    if (typeof slideOptions !== 'object') {
+      action = slideOptions;
+      slideOptions = {};
+    }
+
+
+    var command = action || "";
+    var defaults = {
+      auto: true,
+      wrap: false,
+      timeInterval: 3000,
+      slideLoadingTime: 2000
+    };
+
+    slideOptions = $.extend({}, defaults, slideOptions);
+    // console.log(slideOptions);
+
+    if (command == 'back') {
+      console.log("back back friend");
+
+    } else if (command == 'forward') {
+      console.log("forward ever backward never!");
+
+    } else if (command == 'pause') {
+      console.log("PAUSE!");
+
+    } else if (command == 'cycle') {
+      console.log("cycle through ...");
+
+    } else if (typeof command == 'number') {
+      console.log("A number was passed");
+      //check if the number enter was an integer.
+      // if not an integer throw exception
+
+    } else {
+      setTimeout(function () {
+        slides.filter(".active").toggle();
+        if (slideOptions.auto == true) {
+          interval = setInterval(advance, slideOptions.timeInterval, slides, slideOptions.wrap);
+        }
+      }, slideOptions.slideLoadingTime);
+
+    }
+
+  }
+
+}(jQuery));
+
+function advance(slides, wrap) {
+  var lastIndex = slides.length - 1;
+  var active = slides.filter('.active');
+  var currentIndex = slides.index(active);
+
+  if (currentIndex == lastIndex) {
+    // if wrap is false the slider will not start over
+    if (wrap == true) {
+      var firstSlide = $(slides[0]);
+      moveSlide(active, firstSlide);
+
+    } else {
+      clearInterval(interval);
+    }
+
+  } else {
+    var nextSlide = $( slides[currentIndex + 1] );
+    moveSlide(active, nextSlide);
+  }
+
+  // console.log("wrap is " + wrap);
+  // console.log(slides);
+  // console.log(active);
+  // console.log(currentIndex);
+  // console.log(nextSlide);
+
+}
+
+function moveSlide(activeSlide, slide) {
+  activeSlide.toggle();
+  slide.toggle();
+  activeSlide.removeClass("active");
+  slide.addClass("active");
+}
+
+function background_slideShow() {
+
+}
+
+
+
+
+
